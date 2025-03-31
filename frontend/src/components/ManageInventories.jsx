d9949f286d0b74c0c77978eb6ee311fc97c760bc
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-03-31 16:22:15 +0530

    updated

--- frontend/src/components/ManageInventories.jsx
+++ frontend/src/components/ManageInventories.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBarsProgress, faBucket, faDeleteLeft, faEdit, faRemove } from '@fortawesome/free-solid-svg-icons';
+import { faBarsProgress, faEdit, faRemove } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ManageInventories = () => {
