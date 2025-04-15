7004da0d4caccd2415a720ced379911d98f4f105
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-04-16 00:47:37 +0530

    fix: add missing newline at end of file in catalogRoutes.js

--- backend/routes/catalogRoutes.js
+++ backend/routes/catalogRoutes.js
@@ -6,3 +6,5 @@ const catalogController = require('../controllers/catalogController');
 
 // Route to get all products
 router.get('/inventory', catalogController.getAllProducts);
+
+module.exports = router;
