291df5a4145b4df5ad4d27f031ffc86745e09891
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-04-16 00:43:34 +0530

    feat: implement getAllProducts function to fetch all products

--- backend/controllers/catalogController.js
+++ backend/controllers/catalogController.js
@@ -1,3 +1,14 @@
 // controllers/catalogController.js
 
 const Product = require('../models/catalogModel');
+
+// Fetch all products
+const getAllProducts = async (req, res) => {
+    try {
+      const products = await Product.find(); // Retrieve all products
+      res.status(200).json(products);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  };
