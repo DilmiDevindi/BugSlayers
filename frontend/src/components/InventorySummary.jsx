eeaf3c088f159d91702ad282b75f21253e54c5a6
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-04-20 22:06:36 +0530

    refactor: ensure bootstrap CSS is imported in InventorySummary component

--- frontend/src/components/InventorySummary.jsx
+++ frontend/src/components/InventorySummary.jsx
@@ -14,6 +14,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const InventorySummary = () => {
     return (
