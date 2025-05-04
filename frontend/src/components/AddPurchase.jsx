38b701f18deaa7c744619cd92556a21ed39d2108
Dulangilakshani <dulangilakshani12@gmail.com>
2025-05-04 23:49:44 +0530

    add new button in the AddPurchase.jsx

--- frontend/src/components/AddPurchase.jsx
+++ frontend/src/components/AddPurchase.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./AddPurchases.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf, faCalendarDays, faChartBar, faDownload } from '@fortawesome/free-solid-svg-icons';
+
 
 const AddPurchase = () => {
   const [purchase, setPurchase] = useState({
@@ -77,9 +76,7 @@ const AddPurchase = () => {
           onChange={handleChange}
           required
         />
-        <button type="submit" className="btn btn-primary w-100">
-                  <FontAwesomeIcon icon={faChartBar} className="me-2" />Add New Record
-                </button>
+        <button type="submit">Add New Record</button>
       </form>
     </div>
   );
