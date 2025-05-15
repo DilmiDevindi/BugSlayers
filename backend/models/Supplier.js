c4b7627a84f4b09eb3948c9a7c174cec57b5ee91
Dulangilakshani <dulangilakshani12@gmail.com>
2025-05-15 21:44:10 +0530

    update backend models in supplier.js

--- backend/models/Supplier.js
+++ backend/models/Supplier.js
@@ -1,12 +1,41 @@
 const mongoose = require('mongoose');
 
 const SupplierSchema = new mongoose.Schema({
-  supplierName: String,
-  phone: String,
-  email: String,
-  address: String,
-  supplyProducts: String,
-  paymentTerms: String
+  date: {
+    type: Date,
+    required: true
+  },
+  supplierName: {
+    type: String,
+    required: true
+  },
+  phone1: {
+    type: String,
+    required: true
+  },
+  phone2: {
+    type: String
+  },
+  fax: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  supplyProducts: {
+    type: String,
+    required: true
+  },
+  paymentTerms: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Supplier', SupplierSchema);
+
