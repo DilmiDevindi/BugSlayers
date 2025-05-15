d4c49e511971e17c587df775da3fe5e42b67e6a8
Hiruni <thushanthigithika@gmail.com>
2025-05-16 00:39:30 +0530

    Fix initial state in Login component to display 'Login' instead of 'Sign Up'

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-  const[currentState,setCurrentState] =useState('Sign Up');
+  const[currentState,setCurrentState] =useState('Login');
   return (
     <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
