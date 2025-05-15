6a2c27f84cd3c3a060c987b5146cf429a85ee700
DilmiDevindi <dilmidliyanag@gmail.com>
2025-05-15 22:59:36 +0530

    fix: format total amount in SalesReport PDF export to two decimal places for consistency

--- frontend/src/components/SalesReport.jsx
+++ frontend/src/components/SalesReport.jsx
@@ -253,7 +253,7 @@ const SalesReport = () => {
                   <td>{item._id}</td>
                   <td>{item.totalQuantity}</td>
                   <td>Rs. {item.totalSales.toFixed(2)}</td>
-                  <td>Rs. {item.totalQuantity*item.totalSales}</td>
+                  <td>Rs. {(item.totalQuantity * item.totalSales).toFixed(2)}</td>
                 </tr>
               ))}
             </tbody>
