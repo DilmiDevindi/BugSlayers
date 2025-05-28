4e7a858470d222488d21eb51a594cdbd2c568648
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-05-29 00:04:20 +0530

    refactor: add addSubcategory route to categoryRoutes for improved subcategory management

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,16 +1,16 @@
-// routes/categoryRouter.js
-
 const express = require('express');
 const router = express.Router();
 const {
     getAllCategories,
     addCategory,
     deleteCategory,
-    updateCategory
+    updateCategory,
+    addSubcategory 
 } = require('../controllers/categoryController');
 
 router.get('/', getAllCategories);
 router.post('/add', addCategory);
+router.post('/:categoryId/subcategory', addSubcategory); 
 router.delete('/:id', deleteCategory);
 router.put('/:id', updateCategory);
 
