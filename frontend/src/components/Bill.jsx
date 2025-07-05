5d29701d2b1bfd854702255de278459b2ed16bca
DilmiDevindi <dilmidliyanag@gmail.com>
2025-07-05 19:05:33 +0530

    feat: update API endpoint for fetching item details in BillForm

--- frontend/src/components/Bill.jsx
+++ frontend/src/components/Bill.jsx
@@ -64,7 +64,7 @@ function BillForm() {
 
     if (code) {
       try {
-        const res = await axios.get(`http://localhost:5000/api/bill/item/${code}`);
+        const res = await axios.get(`http://localhost:5000/api/inventryitem/item/${code}`);
         const item = res.data;
         setItemName(item.name || '');
         setItemPrice(parseFloat(item.price).toFixed(2) || '');
