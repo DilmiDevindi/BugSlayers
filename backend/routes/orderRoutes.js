72c33cfd14c13437898a63ffb0d781f4d6af2af4
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-06 18:02:56 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
+  const router = express.Router();
+  const orderController = require('../controllers/orderController');
 
-router.get('/', orderController.getAllOrders);
-router.post('/', orderController.createOrder);
-router.put('/:id', orderController.updateOrder);
+  router.get('/', orderController.getAllOrders);
+  router.post('/', orderController.createOrder);
+  router.put('/:id', orderController.updateOrder);
 
-module.exports = router;
+  // New route for report with date range (kept for potential future use)
+  router.get('/report', orderController.getOrderReport);
+
+  module.exports = router;
