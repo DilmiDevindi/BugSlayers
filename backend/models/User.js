4a5cc12a396eb30c6ae8241f1ad75e5dec974283
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-06 16:35:07 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-  },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
