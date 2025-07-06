56fdc5b2c14d7cf8a21c511c5b3ca1191ae604fd
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-06 14:15:53 +0530

    Update order.js

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,11 +1,23 @@
-// models/orderModel.js
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  orderId: { type: String, required: true, unique: true },
-  quantity: { type: Number, required: true },
-  discount: { type: Number, required: true },
-  date: { type: String, required: true },
+  orderId: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  discount: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
