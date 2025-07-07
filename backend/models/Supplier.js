4b383ed9f215cc2d97dfbb9f02b39640339b2580
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-08 00:09:44 +0530

    Update Supplier.js

--- backend/models/Supplier.js
+++ backend/models/Supplier.js
@@ -1,38 +1,22 @@
 const mongoose = require("mongoose");
 
 const SupplierSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  supplierName: {
-    type: String,
-    required: true,
-  },
-  phone1: {
-    type: String,
-    required: true,
-  },
-  phone2: {
-    type: String,
-  },
-  fax: {
-    type: String,
-  },
-  email: {
-    type: String,
-
-  },
-  address: {
-    type: String,
-  },
-  supplyProducts: {
-    type: [String], // Array of product names
-  },
-  paymentTerms: {
-    type: String,
-
-  },
+  supplierName: { type: String, required: true, unique: true },
+  phone1: { type: String, required: true },
+  phone2: String,
+  fax: String,
+  email: String,
+  address: String,
+  supplyProducts: [
+    {
+      product: String,
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  paymentTerms: String,
 });
 
 module.exports = mongoose.model("Supplier", SupplierSchema);
