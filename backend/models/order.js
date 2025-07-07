2362bb1506512b865f3ed9100da8e4e773296fae
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-08 00:09:36 +0530

    Update order.js

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   orderId: { type: String, required: true, unique: true },
+  companyName: { type: String, required: true }, // ✅ added
   quantity: { type: Number, required: true },
   discount: { type: Number, required: true },
   date: { type: Date, required: true },
