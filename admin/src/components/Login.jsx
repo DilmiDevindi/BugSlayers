da8e3d4c813de03c0421bde8eceecb9a46cee3d2
Hiruni <thushanthigithika@gmail.com>
2025-07-08 12:54:12 +0530

    Enhance Login component structure by adding form elements for email input

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -4,7 +4,16 @@ const Login = () => {
   return (
     <div>
       <div>
-        <h1>Admin Panel</h1></div>
+        <h1>Admin Panel</h1>
+        <form>
+            <div>
+                <p>Email Address</p>
+                <input type="email" placeholder='your@gmail.com' required/>
+                
+            </div>
+        </form>
+
+        </div>
 
     </div>
   )
