f0e7c7ee39a6dffd47fedc34ba562f0b91ee40ba
Hiruni <thushanthigithika@gmail.com>
2025-07-08 13:39:51 +0530

    Enhance styling of email input field in Login component for improved layout

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
       <div>
         <h1>Admin Panel</h1>
         <form>
-            <div>
+            <div className='mb-3 min-w-72'>
                 <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
                 <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@gmail.com' required/>
 
