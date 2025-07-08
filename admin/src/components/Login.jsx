57089fc2bb3212112dae6d5145856639cc784420
Hiruni <thushanthigithika@gmail.com>
2025-07-08 13:03:56 +0530

    Enhance styling of Login component input fields and labels for better user experience

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -7,14 +7,14 @@ const Login = () => {
         <h1>Admin Panel</h1>
         <form>
             <div>
-                <p>Email Address</p>
-                <input type="email" placeholder='your@gmail.com' required/>
-                
+                <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@gmail.com' required/>
+
             </div>
             <div>
-                <p>Email Address</p>
-                <input type="password" placeholder='Enter your Password' required/>
-                
+                <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300' type="password" placeholder='Enter your Password' required/>
+
             </div>
             <button type="Submit">Login</button>
         </form>
