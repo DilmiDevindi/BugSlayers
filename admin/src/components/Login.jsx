c2f605d41055586883a5c5b03595940aa52c3892
Hiruni <thushanthigithika@gmail.com>
2025-07-08 13:42:35 +0530

    Enhance layout of Login component by adding styling to the container

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <div>
-        <h1>Admin Panel</h1>
+      <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+        <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
         <form>
             <div className='mb-3 min-w-72'>
                 <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
