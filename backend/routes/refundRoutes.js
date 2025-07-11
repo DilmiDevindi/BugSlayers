fd178e25b238608c0a368711a6017414136bb55f
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-11 17:13:32 +0530

    Update refundRoutes.js

--- backend/routes/refundRoutes.js
+++ backend/routes/refundRoutes.js
@@ -2,16 +2,9 @@ const express = require("express");
 const router = express.Router();
 const refundController = require("../controllers/refundController");
 
-// GET all refunds
 router.get("/", refundController.getAllRefunds);
-
-// CREATE refund
 router.post("/", refundController.createRefund);
-
-// UPDATE refund
 router.put("/:id", refundController.updateRefund);
-
-// DELETE refund
 router.delete("/:id", refundController.deleteRefund);
 
 module.exports = router;
