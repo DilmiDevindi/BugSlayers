176ba9ea257a14d1d2ff7ab0de257279e9752f67
Dulangilakshani <dulangilakshani12@gmail.com>
2025-07-13 13:49:16 +0530

    Update purchasesRoutes.js

--- backend/routes/purchasesRoutes.js
+++ backend/routes/purchasesRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const PurchaseController = require("../controllers/purchaseController");
 
-router.get("/", PurchaseController.getAllPurchases);
+// Use the correct exported function names here:
+router.get("/", PurchaseController.getPurchases);
 router.post("/", PurchaseController.createPurchase);
 router.put("/:id", PurchaseController.updatePurchase);
 router.delete("/:id", PurchaseController.deletePurchase);
