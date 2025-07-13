a809425705de6924288ce893a19de8416bc5d716
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-07-13 17:12:34 +0530

    updated sidebar and main content to fit with layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,4 +1,3 @@
-
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
@@ -9,8 +8,13 @@ const Layout = () => {
       <Navbar />
       <div className="d-flex" style={{ height: '100vh', overflow: 'hidden' }}>
         <Sidebar />
-        <div className="flex-grow-1 p-4" style={{ overflowY: 'auto', marginTop: '50px' }}>
-          
+        <div 
+          className="flex-grow-1 p-4" 
+          style={{ 
+            overflowY: 'auto', 
+            marginTop: '56px',   // Adjust to your Navbar height
+            marginLeft: '260px'  // Same as Sidebar width
+          }}>
           <Outlet />
         </div>
       </div>
